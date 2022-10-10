0251f508f90982a94297d5a4479139986d3ff8fd
alion <675021955@qq.com>
2022-10-10 16:20:26 +0800

    fix

--- base_library/src/main/java/com/example/base_library/net_1/exception/NetExceptions.kt
+++ base_library/src/main/java/com/example/base_library/net_1/exception/NetExceptions.kt
@@ -1,7 +1,5 @@
 package com.example.base_library.net_1.exception
 
-import com.example.base_library.MyApplication
-import com.example.base_library.R
 import com.example.base_library.net_1.*
 import com.google.gson.JsonParseException
 import com.google.gson.JsonSyntaxException
@@ -15,39 +13,43 @@ import java.text.ParseException
 object NetExceptions {
     fun handleException(e: Throwable): SelfDefineHttpException {
         val ex: SelfDefineHttpException
-        return if (e is HttpException) { //HTTP错误
-            ex = SelfDefineHttpException(e, err_0x0222)
-            ex.setMessage(e.message())
-            ex
-        } else if (e is SelfDefineHttpException) {    //服务器返回的错误
-            val resultException: SelfDefineHttpException = e
-            ex = SelfDefineHttpException(resultException, resultException.state)
-            val msg: String? = resultException.message
-            ex.setMessage(msg)
-            ex
-        } else if (e is JsonParseException //解析错误
-            || e is JSONException
-            || e is ParseException
-            || e is JsonSyntaxException
-        ) {
-            ex = SelfDefineHttpException(e, err_0x0106)
-            ex.setMessage(e.message)
-            ex
-        } else if (e is ConnectException || e is UnknownHostException) {//网络错误
-            ex = SelfDefineHttpException(e, err_0x1111)
-//            ex.setMessage(MyApplication.getInstance().getString(R.string.system_error_title))
-            ex
-        } else if (e is SocketTimeoutException) {
-            ex = SelfDefineHttpException(e, err_0x1111)
-//            ex.setMessage(MyApplication.getInstance().getString(R.string.server_busy))
-            ex
-//        } else if (e is AkamaiChallengeException) { //Akamai challenge异常
-//            ex = SelfDefineHttpException(e, ErrorCode.err_0x428)
-//            ex
-        } else {
-            ex = SelfDefineHttpException(e, err_0x0106)
-//            ex.setMessage(MyApplication.getInstance().getString(R.string.system_error_title)) //未知错误
-            ex
+        //解析错误
+        return when (e) {
+            is HttpException -> { //HTTP错误
+                ex = SelfDefineHttpException(e, err_0x0222)
+                ex.setMessage(e.message())
+                ex
+            }
+            is SelfDefineHttpException -> {    //服务器返回的错误
+                val resultException: SelfDefineHttpException = e
+                ex = SelfDefineHttpException(resultException, resultException.state)
+                val msg: String? = resultException.message
+                ex.setMessage(msg)
+                ex
+            }
+            is JsonParseException, is JSONException, is ParseException, is JsonSyntaxException -> {
+                ex = SelfDefineHttpException(e, err_0x0106)
+                ex.setMessage(e.message)
+                ex
+            }
+            is ConnectException, is UnknownHostException -> {//网络错误
+                ex = SelfDefineHttpException(e, err_0x1111)
+                //            ex.setMessage(MyApplication.getInstance().getString(R.string.system_error_title))
+                ex
+            }
+            is SocketTimeoutException -> {
+                ex = SelfDefineHttpException(e, err_0x1111)
+                //            ex.setMessage(MyApplication.getInstance().getString(R.string.server_busy))
+                ex
+                //        } else if (e is AkamaiChallengeException) { //Akamai challenge异常
+                //            ex = SelfDefineHttpException(e, ErrorCode.err_0x428)
+                //            ex
+            }
+            else -> {
+                ex = SelfDefineHttpException(e, err_0x0106)
+                //            ex.setMessage(MyApplication.getInstance().getString(R.string.system_error_title)) //未知错误
+                ex
+            }
         }
     }
 }
