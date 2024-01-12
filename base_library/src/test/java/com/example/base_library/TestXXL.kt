fc5540ee4237676aaa7acb743314d2f290ddddcb
xiexinli <xiexinli@dhgate.com>
2024-01-12 10:49:16 +0800

    Compose

--- base_library/src/test/java/com/example/base_library/TestXXL.kt
+++ base_library/src/test/java/com/example/base_library/TestXXL.kt
@@ -1,5 +1,9 @@
 package com.example.base_library
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 class TestXXL {
@@ -10,6 +14,28 @@ class TestXXL {
 
     @Test
     fun test2() {
-        val lastChar = "".lastChar()
+        runBlocking {
+            val hotFlow = MutableSharedFlow<Int>()
+
+            launch {
+                repeat(5) {
+                    delay(1000)
+                    hotFlow.emit(it)
+                }
+            }
+
+            // 观察者1
+            hotFlow.collect {
+                println("Observer 1: $it")
+            }
+
+            // 观察者2
+            delay(3000) // 观察者2延迟3秒后订阅
+            hotFlow.collect {
+                println("Observer 2: $it")
+            }
+
+            delay(5000) // 为了保持主线程运行
+        }
     }
 }
